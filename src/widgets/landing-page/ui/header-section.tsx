1f4b3a72a9c3edd4daa90df0e513853d26716123
Shreya-singh-uix <shreya@uixlabs.in>
2025-09-04 16:00:16 +0530

    hero-section

---
+++ src/widgets/landing-page/ui/header-section.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button } from "@/shared/ui/components/button";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="w-full flex items-center justify-between px-12 py-4">
+      <div className="flex items-center gap-4">
+        <span
+          className="text-2xl font-[900] text-[rgb(11, 10, 9)
+]"
+        >
+          Resume Builder
+        </span>
+
+        <div className="flex items-center gap-1 px-2 py-1 bg-[rgb(2,164,79)] text-white rounded-full text-xs font-bold">
+          <span>AI Powered</span>
+          
+          <Image
+            src="/images/auto_awesome.svg"
+            alt="AI"
+            width={14}
+            height={14}
+            className="inline-block"
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center gap-7">
+        <Button
+          variant="ghost"
+          size="sm"
+          className="text-blue-900 hover:text-gray-900 font-semibold text-lg cursor-pointer"
+        >
+          Sign In
+        </Button>
+
+        <Button
+          variant="default"
+          size="default"
+          className="bg-blue-900 hover:bg-blue-700 text-white font-medium p-3 rounded-lg shadow-sm cursor-pointer"
+        >
+          Create My Resume
+        </Button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/widgets/landing-page/ui/landing-page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import HeroSection from "./hero-section";
+import Image from "next/image";
+import Header from "./header-section";
+
+function LandingPage() {
+  return (
+    <div className="relative w-full h-full">
+      <Image
+        src="images/landing-page-bg.svg"
+        alt="Background"
+        fill
+        className="object-cover -z-10"
+        priority
+      />
+        <Header />
+
+      <div
+        className="h-[724px] border-2 border-white rounded-[36px] m-4 mt-0 overflow-hidden"
+        style={{
+          background: "radial-gradient(circle, #ccc 1px, transparent 1px)",
+          backgroundSize: "20px 20px",
+        }}
+      >
+        {" "}
+        <HeroSection />
+      </div>
+
+    </div>
+  );
+}
+
+export default LandingPage;

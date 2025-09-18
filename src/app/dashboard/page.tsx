6fb406d7111a42a0715352509c1bc97e2f94d1c5
Shreya-singh-uix <shreya@uixlabs.in>
2025-09-18 15:03:24 +0530

    build error fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,3 @@
-import React from 'react'
-
-function Dashboard() {
-  return (
-    <div className='text-black'>Hii Shreya!!!</div>
-  )
+export default function Dashboard() {
+  return <div className="text-black">Hii Shreya!!!</div>
 }
-
-export default Dashboard
